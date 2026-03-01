b83ae116fda7f9f555de7083fd2e5bca48040a0b
Thevindu Kevin <thevindukevin@gmail.com>
2026-03-01 17:03:15 +0530

    feat: Add admin page, lesson card component, and marketing footer, along with dependency updates and new production script.

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -6,59 +6,26 @@ export const Footer = () => {
   return (
     <div className="hidden h-20 w-full border-t-2 border-slate-200 p-2 lg:block">
       <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
+        <Button size="lg" variant="ghost" className="w-auto cursor-default">
           <Image
-            src="/hr.svg"
-            alt="Croatian"
+            src="/lk.jpg"
+            alt="Sinhala"
             height={32}
             width={40}
             className="mr-4 rounded-md"
           />
-          Croatian
+          Sinhala
         </Button>
 
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
+        <Button size="lg" variant="ghost" className="w-auto cursor-default">
           <Image
-            src="/es.svg"
-            alt="Spanish"
+            src="/lk.jpg"
+            alt="Tamil"
             height={32}
             width={40}
             className="mr-4 rounded-md"
           />
-          Spanish
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/fr.svg"
-            alt="French"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          French
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/it.svg"
-            alt="Italian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Italian
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/jp.svg"
-            alt="Japanese"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Japanese
+          Tamil
         </Button>
       </div>
     </div>
