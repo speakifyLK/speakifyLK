ee5c3b0cf571226209d5c41c3ff50e320500b021
Sandalika Liyanage <sandalika24@gmail.com>
2026-03-06 05:34:16 +0530

    fix: remove unused desc/asc imports to fix eslint errors

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 
 import { auth } from "@clerk/nextjs/server";
-import { eq, and, desc, asc } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 
 import db from "./drizzle";
 import {
