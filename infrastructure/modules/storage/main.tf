7bae189d21d63252933044ff13b771054eebb0f2
itzzjb <desilvabethmin@gmail.com>
2026-03-23 19:39:44 +0530

    refactor: remove unused lifecycle rule from Google Storage Bucket resource

--- infrastructure/modules/storage/main.tf
+++ infrastructure/modules/storage/main.tf
@@ -3,15 +3,6 @@ resource "google_storage_bucket" "rag_content" {
   location                    = var.region
   uniform_bucket_level_access = true
   force_destroy               = false
-
-  lifecycle_rule {
-    condition {
-      age = 90
-    }
-    action {
-      type = "Delete"
-    }
-  }
 }
 
 resource "google_storage_bucket_iam_member" "rag_content_admin" {
