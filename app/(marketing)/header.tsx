f9a59d1ee851cfe0beed3e1b9f427690707a8060
itzzjb <desilvabethmin@gmail.com>
2026-03-31 20:52:57 +0530

    refactor: remove Banner component and related state management from Header

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 
 import {
   ClerkLoaded,
@@ -14,25 +13,15 @@ import { Loader } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-import Banner from "@/components/banner";
 import { Button } from "@/components/ui/button";
 import { links } from "@/config";
-import { cn } from "@/lib/utils";
 
 export const Header = () => {
   const { isSignedIn } = useAuth();
-  const [hideBanner, setHideBanner] = useState(true);
 
   return (
     <>
-      <Banner hide={hideBanner} setHide={setHideBanner} />
-
-      <header
-        className={cn(
-          "h-20 w-full border-b-2 border-slate-200 px-4",
-          !hideBanner ? "mt-20 sm:mt-16 lg:mt-10" : "mt-0"
-        )}
-      >
+      <header className="h-20 w-full border-b-2 border-slate-200 px-4">
         <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
           <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
             <Image src="/mascot.svg" alt="Mascot" height={40} width={40} />
